5e1b50267f1fe23686d81c570348ad288dc2d9f1
Anthony Castor <Anthony Castor>
2017-05-17 20:54:42 +0800

    Update

---
+++ Assets/_MyAsset/_Script/TimeDelayThenChange.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeDelayThenChange : MonoBehaviour {
+
+	public float TimeDelay; 
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ExecuteAfterTime(TimeDelay));
+	}
+	
+	IEnumerator ExecuteAfterTime(float time)
+	 {
+	     yield return new WaitForSeconds(time);
+	 
+	     // Code to execute after the delay
+	     Application.LoadLevel("Scene2");
+	 }
+}
---
+++ Assets/_MyAsset/_Script/_Facebook/FBGlobalVar.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FBGlobalVar : MonoBehaviour {
+	public static string FBUserID,FBName,FBGender,FBEmailAddress;
+}
